708012ab4f02eccd40c1745999fba4cdc102cdf7
Jeanre Swanepoel <jeanres@icloud.com>
2024-02-23 19:27:20 +0200

    vimwiki

--- home/neovim/after/plugin/vimwiki.lua
+++
@@ -1,9 +0,0 @@
-local icloud_vimwiki_path = "~/Documents/vimwiki"
-
-vim.g.vimwiki_list = {
-  {
-    path = icloud_vimwiki_path,
-    syntax = 'markdown',  -- or 'media' based on your preference
-    ext = '.md',
-  }
-}
--- home/neovim/init.lua
+++ home/neovim/init.lua
@@ -1,2 +1,12 @@
 require('core')
 require('plugins')
+
+local icloud_vimwiki_path = "~/Documents/vimwiki"
+
+vim.g.vimwiki_list = {
+  {
+    path = icloud_vimwiki_path,
+    syntax = 'markdown',  -- or 'media' based on your preference
+    ext = '.md',
+  }
+}
