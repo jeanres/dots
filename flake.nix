0a7ce5adbbb9c9477f2d0e20bf9b29d2f3cad819
Jeanre Swanepoel <jeanres@icloud.com>
2024-03-11 16:19:17 +0200

    null ls

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "Jeanre's system configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     darwin = {
       url = "github:lnl7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
